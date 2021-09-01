faa3a2e93a29ace3a7993745cd15c3cf525076d1
romax911 <Romax911@mail.com>
2021-09-02 01:11:47 +0300

    +Add api v3

--- api/init.sh
+++ api/init.sh
@@ -10,7 +10,7 @@ pip3 install gunicorn
 pip install hurry.filesize
 pip3 install hurry.filesize
 
-gunicorn -w 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:80 api:app
-
 crontab -l | { cat; echo "0 */6 * * * /home/ubuntu/TM_ShadowsocksR-main/api/python3 parser.py  >> /home/ubuntu/api/parser.log 2>&1"; } | crontab -
 crontab -l | { cat; echo "0 */24 * * * /home/ubuntu/TM_ShadowsocksR-main/api/restart_api >> /home/ubuntu/api/api.log 2>&1"; } | crontab -
+
+gunicorn -w 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:80 api:app
